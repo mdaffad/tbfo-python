aec561b4f3cf20c97bc43d6422fb7679763abc83
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2019-09-28 12:01:29 +0700

    tidur dan makan

--- DFA.py
+++ DFA.py
@@ -1,6 +1,20 @@
+class DFA:
+    def __init__ (self, energy = 0, hygiene = 0, fun = 0):
+    
+    def Tidur(self, a):
+        if a == "Siang":
+            self.energy +=10
+        elif a == "Malam":
+            self.energy +=15
+    
+    def Makan(self, a):
+        if a == "Hamburger":
+            self.energy += 5
+        elif a == "Pizza":
+            self.energy += 10
+        elif a == "Steak and Beans":
+            self.energy += 15
+
 txt = str(raw_input())
-
-
-x = txt.split()
-
+x = txt.split(,3)
 print(x)
