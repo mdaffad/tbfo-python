c8d941be089e338861a189b715dafbe5e91f3017
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2019-09-28 12:47:46 +0700

    fungsi tabel di class dfa

--- DFA.py
+++ DFA.py
@@ -14,7 +14,49 @@ class DFA:
             self.energy += 10
         elif a == "Steak and Beans":
             self.energy += 15
-
+    def Minum(self, a):
+        if a == "Air":
+            self.hygiene -= 5
+        elif a == "Kopi":
+            self.energy += 5
+            self.hygiene-= 10
+        elif a == "Jus":
+            self.energy += 10
+            self.hygiene-=5
+    
+    def Buang (self, a):
+        if a == "Kecil":
+            self.hygiene+=5
+        elif a == "Besar"
+            self.Besar +=10
+            self.energy+=-5
+    def Bersosialisasi(self):
+        self.fun += 15
+        self.energy-=10
+        self.hygiene-=5
+    def Bermain(self, a);
+        if a == "Media":
+            self.fun +=10
+            self.energy-=10
+        elif a == "Komputer":
+            self.fun +=15
+            self.energy-=10
+    def Mandi (self):
+        self.hygiene+=15
+        self.energy -=5
+    def Cuci (self):
+        self.hygiene+=5
+    def Mendengarkan (self)
+        self.fun += 10
+        self.energy -= 5
+    def Membaca(self,a):
+        if a == "Koran":
+            self.fun+=5
+            self.energy-=5
+        elif a == "Novel":
+            self.fun += 10
+            self.energy -=5
+  
 txt = str(raw_input())
 x = txt.split(,3)
 print(x)
