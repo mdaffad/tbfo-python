99b836c5dca1faec53c3ccff5389681d31133318
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2019-09-28 16:38:14 +0700

    iter

--- iter.py
+++ iter.py
@@ -1,5 +1,6 @@
 import csv
 import itertools
+import DFA
 
 A = [0, 5, 10,15]
 B = [0, 5, 10,15]
@@ -8,10 +9,12 @@ C = [0, 5, 10,15]
 a = [A, B, C]
 data = list(itertools.product(*a))
 
-
-
 with open('dataset.csv','wb') as out:
     csv_out=csv.writer(out)
     csv_out.writerow(['energy','hygiene','fun'])
     for row in data:
         csv_out.writerow(row)
+
+
+
+
