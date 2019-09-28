2f959fe17ac70823c5eca964db5310865e880316
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2019-09-28 15:55:14 +0700

    nambah dataset

---
+++ iter.py
@@ -0,0 +1,17 @@
+import csv
+import itertools
+
+A = [0, 5, 10,15]
+B = [0, 5, 10,15]
+C = [0, 5, 10,15]
+
+a = [A, B, C]
+data = list(itertools.product(*a))
+
+
+
+with open('dataset.csv','wb') as out:
+    csv_out=csv.writer(out)
+    csv_out.writerow(['energy','hygiene','fun'])
+    for row in data:
+        csv_out.writerow(row)
